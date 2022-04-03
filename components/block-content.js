fbb52731adb2348a5aef84fe8e58ca27bc3ca137
Philip Kvam <ph_kvam@hotmail.com>
2022-04-03 12:37:44 +0200

    framer motion

--- components/block-content.js
+++ components/block-content.js
@@ -3,17 +3,47 @@ import cx from 'classnames'
 import BlockContent from '@sanity/block-content-to-react'
 
 import { blockSerializers } from '@components/block-serializers'
+import { AnimatePresence, m } from 'framer-motion'
+
+const fadeAnim = {
+  show: {
+    opacity: 0,
+    y: -100,
+    transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.75 }
+  },
+  hide: {
+    opacity: 0,
+    y: -100,
+    transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.85 }
+  },
+}
 
 const Content = ({ blocks, className }) => {
   if (!blocks) return null
 
   return (
+    <m.div
+    initial="hide"
+    animate="show"
+    exit="hide"
+    variants={fadeAnim}
+    whileInView={{
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.3,
+        ease: [0.6, 0.01, -0.05, 0.95],
+        duration: 1.2,
+      },
+    }}
+  >
     <BlockContent
       renderContainerOnSingleChild
       className={cx('rc', className)}
       blocks={blocks}
       serializers={blockSerializers}
     />
+        </m.div>
   )
 }
 
--- components/modules/divider-photo.js
+++ components/modules/divider-photo.js
@@ -1,6 +1,20 @@
 import React from 'react'
 
 import Photo from '@components/photo'
+import { AnimatePresence, m } from 'framer-motion'
+
+const fadeAnim = {
+  show: {
+    opacity: 0,
+    y: -100,
+    transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.75 }
+  },
+  hide: {
+    opacity: 0,
+    y: -100,
+    transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.85 }
+  },
+}
 
 const DividerPhoto = ({ data = {} }) => {
   const { photo } = data
@@ -9,12 +23,28 @@ const DividerPhoto = ({ data = {} }) => {
 
   return (
     <div className="divider">
+        <m.div
+    initial="hide"
+    animate="show"
+    exit="hide"
+    variants={fadeAnim}
+    whileInView={{
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.3,
+        ease: [0.6, 0.01, -0.05, 0.95],
+        duration: 1.2,
+      },
+    }}
+  >
       <Photo
         photo={photo}
         width={1600}
         srcSizes={[800, 1000, 1200, 1600]}
         sizes="100vw"
       />
+      </m.div>
     </div>
   )
 }
